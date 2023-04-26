b0cf8aa2783df9630627f7bde6177b1d81bb6b76
Fabrice <dupont.fabrice62@gmail.com>
2023-04-26 09:40:54 +0200

    script file renamed

--- cypress/e2e/test-register-form.cy.js
+++ cypress/e2e/test-register-form.cy.js
@@ -24,3 +24,27 @@ describe('inscription réussie', () => {
     cy.get('[data-qa="signup-submit-button"]').click()
   })
 })
+describe('inscription échouée', () => {
+  // it('effacer les cookies', () => {
+  //   cy.clearCookies()
+  // })
+  it('visite du site web', () => {
+    cy.visit('https://preprod.backmarket.fr/fr-fr/register')
+  })
+
+  it('entre un prénom', () => {
+    cy.get('#firstName').type('Guy')
+  })
+  it('entre un nom', () => {
+    cy.get('#lastName').type('Teub')
+  })
+  it('entre une adresse email', () => {
+    cy.get('#signup-email').type('test@testeur2000.fr')
+  })
+  it('entre un mot de passe trop court', () => {
+    cy.get('#signup-password').type('abcde')
+  })
+  it('clique sur le bouton submit', () => {
+    cy.get('[data-qa="signup-submit-button"]').click()
+  })
+})
