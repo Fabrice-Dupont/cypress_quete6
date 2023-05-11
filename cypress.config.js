354338f363b38670298e607d77041cb3cc122809
Fabrice Dupont <dupont.fabrice62@gmail.com>
2023-05-11 15:30:25 +0200

    adding connection with the new password

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,18 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  projectId: '6m5idd',
+  env: {
+    MAILSLURP_API_KEY: "593a3b117cc273c364f0693b6a040b56261a99644023ae9475dd5fe69dc61251",
+  },
+  reporter: "cypress-mochawesome-reporter",
 
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
-    }
-  }
-})
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+    defaultCommandTimeout: 40000,
+    responseTimeout: 40000,
+    requestTimeout: 40000,
+  },
+});
--- cypress/e2e/test-register-form.cy.js
+++ cypress/e2e/test-register-form.cy.js
@@ -1,91 +1,85 @@
-/// <reference types="cypress" />
+/// <reference types="cypress-mailslurp" />
 
-import { faker } from '@faker-js/faker'
-const registeredEmail = faker.internet.email()
-const registerdPassword = faker.internet.password(15, false, /[a-zA-Z0-9]/)
+import { faker } from "@faker-js/faker";
+const registerdPassword = faker.internet.password(15, false, /[a-zA-Z0-9]/);
+const newPassword = faker.internet.password(15, false, /[a-zA-Z0-9]/);
 
-describe('inscription réussie', () => {
-  beforeEach(() => {
-    cy.clearCookies()
-    cy.visit('https://preprod.backmarket.fr/fr-fr/register')
-  })
-  it('remplissage correct du formulaire et soumission', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type(faker.name.firstName())
-    cy.get('#lastName').type(faker.name.lastName())
-    cy.get('#signup-email').type(registeredEmail)
-    cy.get('#signup-password').type(registerdPassword)
-    cy.get('[data-qa="signup-submit-button"]').click()
-    cy.wait(5000)
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/dashboard')
-    })
-  })
-})
+let emailAddress;
+let inboxId;
+//const resetPasswordUrlRegex = /https:\/\/.*\/clicks.backmarket.com.*/;
 
-describe('inscription échouée', () => {
+describe("basic usage", function () {
+  it("can load the plugin", async function () {
+    // test we can connect to mailslurp
+    const mailslurp = await cy.mailslurp();
+    const userInfo = await mailslurp.userController.getUserInfo();
+    expect(userInfo.id).to.exist;
+  });
+});
+describe("création d'une nouvelle adresse email", function () {
+  before(function () {
+    return cy
+      .mailslurp()
+      .then((mailslurp) => mailslurp.createInbox())
+      .then((inbox) => {
+        // save inbox id and email address to this (make sure you use function and not arrow syntax)
+        cy.wrap(inbox.id).as("inboxId");
+        cy.wrap(inbox.emailAddress).as("emailAddress");
+      });
+  });
+  it("stockage dans des variables", function () {
+    // get wrapped email address and assert contains a mailslurp email address
+    expect(this.emailAddress).to.contain("@mailslurp");
+    emailAddress = this.emailAddress;
+    inboxId = this.inboxId;
+  });
+});
+
+describe("inscription avec une addresse mailslurp", () => {
   beforeEach(() => {
-    cy.clearCookies()
-    cy.visit('https://preprod.backmarket.fr/fr-fr/register')
-  })
-  it('remplissage du formulaire avec mot de passe trop court et soumission', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type(faker.name.firstName())
-    cy.get('#lastName').type(faker.name.lastName())
-    cy.get('#signup-email').type(faker.internet.email())
-    cy.get('#signup-password').type(faker.internet.password(7))
-    cy.get('[data-qa="signup-submit-button"]').click()
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/register')
-    })
-  })
-  it('remplissage du formulaire avec email invalide et soumission', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type(faker.name.firstName())
-    cy.get('#lastName').type(faker.name.lastName())
-    cy.get('#signup-email').type(faker.name.fullName())
-    cy.get('#signup-password').type(
-      faker.internet.password(8, false, /[a-zA-Z0-9]/)
-    )
-    cy.get('[data-qa="signup-submit-button"]').click()
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/register')
-    })
-  })
-  it('remplissage du formulaire sans le prénom et soumission', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#lastName').type(faker.name.lastName())
-    cy.get('#signup-email').type(faker.internet.email())
-    cy.get('#signup-password').type(faker.internet.password(8))
-    cy.get('[data-qa="signup-submit-button"]').click()
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/register')
-    })
-  })
-  it('remplissage du formulaire sans le nom et soumission', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#firstName').type(faker.name.firstName())
-    cy.get('#signup-email').type(faker.internet.email())
-    cy.get('#signup-password').type(faker.internet.password(8))
-    cy.get('[data-qa="signup-submit-button"]').click()
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/register')
-    })
-  })
-})
-describe('Authentification', () => {
-  beforeEach(() => {
-    cy.clearCookies()
-    cy.visit('https://preprod.backmarket.fr/fr-fr/register')
-  })
-  it('authentification réussie avec derniers identifiants corrects', () => {
-    cy.get('[data-qa="accept-cta"]').click()
-    cy.get('#signin-email').type(registeredEmail)
-    cy.get('#signin-password').type(registerdPassword)
+    cy.clearCookies();
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+  });
+  it("remplissage correct du formulaire et soumission", () => {
+    cy.get('[data-qa="accept-cta"]').click();
+    cy.get("#firstName").type(faker.name.firstName());
+    cy.get("#lastName").type(faker.name.lastName());
+    cy.get("#signup-email").type(emailAddress);
+    cy.get("#signup-password").type(registerdPassword);
+    cy.get('[data-qa="signup-submit-button"]').click();
+    cy.wait(5000);
+    cy.url().then((actualUrl) => {
+      expect(actualUrl).to.include("/dashboard");
+    });
+  });
+});
+describe("mot de passe oublie", function () {
+  beforeEach(function () {
+    cy.clearCookies();
+    cy.visit("https://preprod.backmarket.fr/fr-fr/register");
+    cy.get('[data-qa="accept-cta"]').click();
+  });
+  it("demande de reinitialisation mot de passe", function () {
+    cy.get("form > :nth-child(3) > .cK_xUFG6").click();
+    cy.get("#email").type(emailAddress);
+    cy.get('[data-test="password-reset-submit-button"]').click();
+    cy.mailslurp().then((mailslurp) =>
+      mailslurp.waitForLatestEmail(inboxId).then((email) => {
+        cy.document().invoke("write", email.body);
+      })
+    );
+    cy.get(".t_pt20px > a").click();
+    cy.get("#newPassword").type(newPassword);
+    cy.get("#newPasswordConfirmation").type(newPassword);
+    cy.get("._1xMx-RYw").click();
+  });
+  it("connexion avec nouveau mot de passe", () => {
+    cy.get("#signin-email").type(emailAddress);
+    cy.get("#signin-password").type(newPassword);
     cy.get('[data-qa="signin-submit-button"]').click()
-    cy.wait(5000)
-    cy.url().then(actualUrl => {
-      expect(actualUrl).to.include('/dashboard')
-    })
-  })
-})
+    cy.wait(5000);
+    cy.url().then((actualUrl) => {
+      expect(actualUrl).to.include("/dashboard");
+    });
+  });
+});
